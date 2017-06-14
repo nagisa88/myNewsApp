d56b5c6eee5f10ae2c8798acdf219e2742aeaed5
kaname88 <nagissa793@gmail.com>
2017-06-14 22:56:45 +0900

    inital commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
+const ApiKey = '2eae69012e444624a393fe412c7ba1b6'
+const SECTIONS = "home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world"; // From NYTimes
+
+
+
+Vue.component('news-list', {
+  props: ['results'],
+  template: `
+    <section>
+      <div class="row" v-for="posts in processedPosts">
+        <div class="columns large-3 medium-6" v-for="post in posts">
+          <div class="card">
+          <div class="card-divider">
+          {{ post.title }}
+          </div>
+          <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
+          <div class="card-section">
+            <p>{{ post.abstract }}</p>
+          </div>
+        </div>
+        </div>
+      </div>
+  </section>
+  `,
+  computed: {
+    processedPosts() {
+      let posts = this.results;
+
+      posts.map(post => {
+        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
+        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
+      });
+
+      let i, j, chunkedArray = [], chunk = 4;
+      for (i=0, j=0; i < posts.length; i += chunk, j++) {
+        chunkedArray[j] = posts.slice(i,i+chunk);
+      }
+      return chunkedArray;
+    }
+  }
+});
+
+
+function buildUrl (url) {
+    return NYTBaseUrl + url + ".json?api-key=" + ApiKey
+}
+
+const vm = new Vue({
+  el: '#newsApp',
+  data: {
+    results: [],
+    sections: SECTIONS.split(', '), 
+    section: 'home', // set default section to 'home'
+  },
+  mounted () {
+    this.getPosts('home');
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section);
+      axios.get(url).then((response) => {
+        this.results = response.data.results;
+      }).catch( error => { console.log(error); });
+    }
+  }
+});
+
+
+
